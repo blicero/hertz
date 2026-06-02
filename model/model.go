ba5df7bef9df31035bc1b321c2533cd1f12ad817
Benjamin Walkenhorst <krylon@posteo.de>
2026-06-02 11:45:07 +0200

    model - add JSON tags to FreqRecord

--- model/model.go
+++ model/model.go
@@ -2,15 +2,16 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 30. 05. 2026 by Benjamin Walkenhorst
 // (c) 2026 Benjamin Walkenhorst
-// Time-stamp: <2026-05-30 11:52:10 krylon>
+// Time-stamp: <2026-06-02 10:55:22 krylon>
 
 // Package model provides data types used throughout the application.
 package model
 
 import "time"
 
+// FreqRecord contains the CPU frequency at a given point in time.
 type FreqRecord struct {
-	ID        int64
-	Timestamp time.Time
-	Freq      []int64
+	ID        int64     `json:"id"`
+	Timestamp time.Time `json:"timestamp"`
+	Freq      []int64   `json:"freq"`
 }
