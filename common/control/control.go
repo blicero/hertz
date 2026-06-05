2493023f8e5f8355b31562f23d3afc5f133c958b
Benjamin Walkenhorst <krylon@posteo.de>
2026-06-05 19:02:19 +0200

    control - export fields in Message

--- common/control/control.go
+++ common/control/control.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 05. 06. 2026 by Benjamin Walkenhorst
 // (c) 2026 Benjamin Walkenhorst
-// Time-stamp: <2026-06-05 15:28:17 krylon>
+// Time-stamp: <2026-06-05 19:02:07 krylon>
 
 //go:generate stringer -type=Command
 
@@ -19,6 +19,6 @@ const (
 
 // Message is a control message.
 type Message struct {
-	cmd     Command
-	payload string
+	Cmd     Command
+	Payload string
 }
